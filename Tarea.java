2d8ba1c4cd904dc264f70e2817ae5fee377ed971
LuisGonzalez93 <lg02393@gmail.com>
2016-12-15 12:13:30 +0100

    Añadido metodo Tareas

---
+++ Tarea.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+/**
+ * Clase Tarea.
+ * 
+ * @author (Luis González de Haro) 
+ * @version (15/12/2016)
+ */
+public class Tarea
+{
+    //Colección
+    private ArrayList<String> tareas;
+    private ArrayList<Boolean> tareaTerminada;
+    
+
+    
+    /**
+     * Constructor
+     */
+
+    public void Tareas(){
+    tareas = new ArrayList<String>();
+    tareaTerminada = new ArrayList<Boolean>();
+    }
+    
+    
+   
+    }
+
